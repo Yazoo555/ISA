ae4c3a87f3e21fdf0815822af149909a19d365c6
Yazoo555 <yajjuchansi@gmail.com>
2025-04-09 18:31:44 +0545

    added innerHTML instead of textContemt

--- script.js
+++ script.js
@@ -13,33 +13,34 @@ const elements = {
 };
 
 document.getElementById("date").textContent = new Date().toDateString();
+const API_key = "2f784d9bff56f9b8cd741b329a162245";
 
 async function getWeather(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2f784d9bff56f9b8cd741b329a162245&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`
     );
 
     const data = await response.json();
     console.log(data);
 
-    elements.city.textContent = `${data.name}, ${data.sys.country}`;
-    elements.temp.textContent = `${data.main.temp}°C`;
-    elements.desc.textContent = data.weather[0].description;
-    elements.press.textContent = `${data.main.pressure} hPa`;
-    elements.humid.textContent = `${data.main.humidity}%`;
-    elements.wind.textContent = `${data.wind.speed} m/s`;
-    elements.dir.textContent = `${data.wind.deg}°`;
+    elements.city.innerHTML = `${data.name}, ${data.sys.country}`;
+    elements.temp.innerHTML = `${data.main.temp}°C`;
+    elements.desc.innerHTML = data.weather[0].description;
+    elements.press.innerHTML = `${data.main.pressure} hPa`;
+    elements.humid.innerHTML = `${data.main.humidity}%`;
+    elements.wind.innerHTML = `${data.wind.speed} m/s`;
+    elements.dir.innerHTML = `${data.wind.deg}°`;
     elements.icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
   } catch (error) {
-    elements.city.textContent = "City not found";
+    elements.city.innerHTML = "City not found";
   }
 }
 
 elements.btn.onclick = function () {
   if (elements.input.value.trim()) getWeather(elements.input.value.trim());
   else {
-    elements.city.textContent = "Please enter a city name";
+    elements.city.innerHTML = "Please enter a city name";
   }
 };
 
